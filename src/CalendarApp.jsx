c07049962d273b947e498d7ea6a122e853ac8769
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-02-14 00:51:12 -0600

    Agregando rutas de la aplicación

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,6 +1,10 @@
+import { BrowserRouter } from "react-router"
+import { AppRouter } from "./router"
 
 export const CalendarApp = () => {
   return (
-    <h1>CalendarApp</h1>
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
   )
 }
