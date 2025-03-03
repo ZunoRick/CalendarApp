795605723b14b911d3bb12c7b15556e9a40c3923
ZunoRick <zunosan.ricardo506@gmail.com>
2025-03-03 14:40:31 -0600

    Iniciando pruebas en FabDelete

---
+++ tests/calendar/components/FabDelete.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import { FabDelete } from "../../../src/calendar/components/FabDelete";
+
+describe('pruebas en el componente', () => {
+  test('debe de mostrar el componente correctamente', () => {
+    render(<FabDelete/>)
+    // screen.debug()
+  })
+});
